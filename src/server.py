f39bb0a4dfb419f4ea93e1ab337c5792d6f067d6
Thamalee <thamaleeuw@gmail.com>
2025-03-20 20:39:43 -0700

    Use Render environment variable for port

--- src/server.py
+++ src/server.py
@@ -61,5 +61,5 @@ def calculate_angle(a, b, c):
     return angle
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
