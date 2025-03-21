900ec7f8e619d39fba8ce865ef97cecdca1b4108
Thamalee <thamaleeuw@gmail.com>
2025-03-20 20:49:31 -0700

    Correct port binding issue clearly for Render

--- src/server.py
+++ src/server.py
@@ -61,5 +61,5 @@ def calculate_angle(a, b, c):
     return angle
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
